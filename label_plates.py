5da4d6bada802dade7a51b1a195d816a6837dcd4
Mack Wilson <mack.wilson2795@gmail.com>
2022-12-06 10:01:56 -0800

    Wrote a simple script to label licence plates

---
+++ label_plates.py
@@ -0,0 +1,21 @@
+#! /usr/bin/env python3
+import cv2
+import os
+import sys
+
+FILE_PATH = "/home/fizzer/plate_images"
+DEST_PATH = "/home/fizzer/labelled_plates"
+
+def main(args):
+    file_list = os.listdir(FILE_PATH)
+    for file in file_list:
+        img = cv2.imread(f"{FILE_PATH}/{file}")
+        cv2.imshow(file, img)
+        label = input("Plate label: ")
+        # TODO: slice and join
+        output_name = f"{file[:-4]}_{label}.png"
+        print(output_name)
+        cv2.imwrite(f"{DEST_PATH}/{output_name}", img)
+
+if __name__ == '__main__':
+    main(sys.argv)
